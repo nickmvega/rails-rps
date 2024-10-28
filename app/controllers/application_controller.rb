ececb2e39370340aced878d687ea03f2090e3fa4
Nicholas Vega <122232969+nickmvega@users.noreply.github.com>
2024-10-28 20:15:37 +0000

    hw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,40 @@
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
+  def home
+    render({ template: "layouts/home" })
+  end
+
+  def rock
+    @user_move = "rock"
+    move
+  end
+
+  def paper
+    @user_move = "paper"
+    move
+  end
+
+  def scissors
+    @user_move = "scissors"
+    move
+  end
+
+  def move
+    @computer_move = ["rock", "paper", "scissors"].sample
+    @result = winner(@user_move, @computer_move)
+    render({ template: "layouts/move" })
+  end
+
+  def winner(user_move, computer_move)
+    if user_move == computer_move
+      return "We tied!"
+    end
+  
+    if (user_move == "rock" && computer_move == "scissors") ||
+       (user_move == "paper" && computer_move == "rock") ||
+       (user_move == "scissors" && computer_move == "paper")
+      "We won!"
+    else
+      "We lost!"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "application", :action => "home"})
+  get("/rock", { :controller => "application", :action => "rock"})
+  get("/paper", { :controller => "application", :action => "paper"})
+  get("/scissors", { :controller => "application", :action => "scissors"})
 end
